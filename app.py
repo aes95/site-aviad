4a9585b325523ec0c7a8e4850692994793a52835
Your Name <you@example.com>
2018-12-14 08:57:13 -0500

    Footer fix

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request
+import os
 
 app = Flask(__name__)
 
@@ -23,13 +24,24 @@ def contact():
 
 
 @app.route("/confirmation", methods=["GET","POST"])
-def confirm():
+def confirmation():
     if request.method == "GET":
         return "Please submit form instead"
-    else:
-        email = request.form.get("email")
-        message = request.form.get("message")
-        return render_template("confirmation.html", message=message)
+    message = request.form.get("message")
+    return render_template("confirmation.html", message=message)
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 
 
